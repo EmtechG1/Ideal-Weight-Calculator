0fbed3c6e78b19b1d8a15d3e0b7f9fff7bdad37d
EmtechG1 <emtechpt96@gmail.com>
2021-04-22 13:42:34 +0800

    1 file modified

--- pages/contact/contact.ts
+++ pages/contact/contact.ts
@@ -8,6 +8,7 @@ import { NavController } from 'ionic-angular';
 export class ContactPage {
   feetHeight: number;
   inchesHeight: number;
+  calcNum: number;
   resultRobinson: number;
   resultMiller: number;
   resultHamwi: number;
@@ -15,11 +16,24 @@ export class ContactPage {
   gender: string;
 
   calculateIWC() {
+    this.calcNum = ((this.feetHeight-5)*12)+this.inchesHeight;
     if (this.gender = "m") {
+      if (this.feetHeight <= 5){
+        this.resultRobinson = 52.0;
+        this.resultMiller = 56.2;
+        this.resultHamwi = 48.0;
+        this.resultDevine = 50.0;
+      } else{
       // Robinson formula: 52 kg + 1.9 kg per every inch over 5 feet
+      this.resultRobinson = 52 + (1.9*this.calcNum);
       //Miller formula: 56.2 kg + 1.41 kg per every inch over 5 feet
+      this.resultMiller = 56.2 + (1.41*this.calcNum);
       // Hamwi formula: 48.0 kg + 2.7 kg per every inch over 5 feet
+      this.resultHamwi = 48.0 + (2.7*this.calcNum);
       //Devine formula: 50.0 kg + 2.3 kg per every inch over 5 feet
+      this.resultDevine = 50.0 + (2.3*this.calcNum);
+      }
+      
 
   	} else if (this.gender= "f") {
       // Robinson formula: 49 kg + 1.7 kg per every inch over 5 feet
