f189c5881ae2d370a87c222a5437461bc3d33b94
EmtechG1 <emtechpt96@gmail.com>
2021-04-22 13:47:28 +0800

    2 files modified

--- pages/contact/contact.ts
+++ pages/contact/contact.ts
@@ -24,25 +24,34 @@ export class ContactPage {
         this.resultHamwi = 48.0;
         this.resultDevine = 50.0;
       } else{
-      // Robinson formula: 52 kg + 1.9 kg per every inch over 5 feet
-      this.resultRobinson = 52 + (1.9*this.calcNum);
-      //Miller formula: 56.2 kg + 1.41 kg per every inch over 5 feet
-      this.resultMiller = 56.2 + (1.41*this.calcNum);
-      // Hamwi formula: 48.0 kg + 2.7 kg per every inch over 5 feet
-      this.resultHamwi = 48.0 + (2.7*this.calcNum);
-      //Devine formula: 50.0 kg + 2.3 kg per every inch over 5 feet
-      this.resultDevine = 50.0 + (2.3*this.calcNum);
+        // Robinson formula: 52 kg + 1.9 kg per every inch over 5 feet
+        this.resultRobinson = 52 + (1.9*this.calcNum);
+        //Miller formula: 56.2 kg + 1.41 kg per every inch over 5 feet
+        this.resultMiller = 56.2 + (1.41*this.calcNum);
+        // Hamwi formula: 48.0 kg + 2.7 kg per every inch over 5 feet
+        this.resultHamwi = 48.0 + (2.7*this.calcNum);
+        //Devine formula: 50.0 kg + 2.3 kg per every inch over 5 feet
+        this.resultDevine = 50.0 + (2.3*this.calcNum);
       }
       
 
   	} else if (this.gender= "f") {
-      // Robinson formula: 49 kg + 1.7 kg per every inch over 5 feet
-      //Miller formula: 53.1 kg + 1.36 kg per every inch over 5 feet
-      //Hamwi formula: 45.5 kg + 2.2 kg per every inch over 5 feet
-      //Devine formula: 45.5 kg + 2.3 kg per every inch over 5 feet
-
-    } else {
-   
+      if (this.feetHeight <= 5){
+        this.resultRobinson = 49.0;
+        this.resultMiller = 53.1;
+        this.resultHamwi = 45.5;
+        this.resultDevine = 45.5;
+      } else{
+        // Robinson formula: 49 kg + 1.7 kg per every inch over 5 feet
+        this.resultRobinson = 49.0 + (1.7*this.calcNum);
+        //Miller formula: 53.1 kg + 1.36 kg per every inch over 5 feet
+        this.resultMiller = 53.1 + (1.36*this.calcNum);
+        //Hamwi formula: 45.5 kg + 2.2 kg per every inch over 5 feet
+        this.resultHamwi = 45.5 + (2.2*this.calcNum);
+        //Devine formula: 45.5 kg + 2.3 kg per every inch over 5 feet
+        this.resultDevine = 45.5 + (2.3*this.calcNum);
+      }
+      
     }
   }
   constructor(public navCtrl: NavController) {
