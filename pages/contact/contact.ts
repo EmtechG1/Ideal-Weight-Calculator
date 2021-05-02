7444b8d0f0ab725ef8416e1a9746c5963638a8bd
EmtechG1 <emtechpt96@gmail.com>
2021-05-02 21:02:37 +0800

    2 files modified

--- pages/contact/contact.ts
+++ pages/contact/contact.ts
@@ -26,28 +26,36 @@ export class ContactPage {
       this.tooLow = "The calculations are only accurate for those who are at least 5 ft tall.";
     }else if ((parseInt(this.feetHeight)>7)){
       this.tooLow = "Are you sure? The tallest person in the world, Robert Wadlow, is 8 feet and 11.1 inches";
-    }else if ((this.gender = "m")) {
+    }else if (this.gender = "m") {
        this.calcNum =parseInt(this.inchesHeight) + (this.feetConvert - this.subtract) ;
-        // Robinson formula: 52 kg + 1.9 kg per every inch over 5 feet 
-        this.resultRobinson =parseFloat(52 + (1.9 * this.calcNum).toFixed(2)) ;
-        //Miller formula: 56.2 kg + 1.41 kg per every inch over 5 feet
-        this.resultMiller =parseFloat(56.2 + (1.41 * this.calcNum).toFixed(2)) ;
-        // Hamwi formula: 48.0 kg + 2.7 kg per every inch over 5 feet
-        this.resultHamwi =parseFloat(48.0 + (2.7 * this.calcNum).toFixed(2));
+       //Miller formula: 56.2 kg + 1.41 kg per every inch over 5 feet
+        this.resultMiller = 56.2 + (1.41 * this.calcNum);
+        this.resultMiller = parseFloat(this.resultMiller.toFixed(2));
+        // Robinson formula: 52 kg + 1.9 kg per every inch over 5 feet
+        this.resultRobinson = 52 + (1.9 * this.calcNum);
+        this.resultRobinson = parseFloat(this.resultRobinson.toFixed(2));
         //Devine formula: 50.0 kg + 2.3 kg per every inch over 5 feet
-        this.resultDevine =parseFloat( 50.0 + (2.3 * this.calcNum).toFixed(2));
-        this.tooLow = `${this.calcNum}`;
-    }else if ((this.gender = "f")) {
+        this.resultDevine = 50.0 + (2.3 * this.calcNum);
+        this.resultDevine = parseFloat(this.resultDevine.toFixed(2));
+        // Hamwi formula: 48.0 kg + 2.7 kg per every inch over 5 feet
+        this.resultHamwi = 48.0 + (2.7 * this.calcNum);
+        this.resultHamwi = parseFloat(this.resultHamwi.toFixed(2));
+        this.tooLow = `m${this.calcNum}`;
+    }else if (this.gender = "f") {
       this.calcNum =parseInt(this.inchesHeight) + (this.feetConvert - this.subtract) ;
-        // Robinson formula: 49 kg + 1.7 kg per every inch over 5 feet parseFloat(.toFixed())
-        this.resultRobinson =parseFloat(49.0 + (1.7 * this.calcNum).toFixed(2)) ;
         //Miller formula: 53.1 kg + 1.36 kg per every inch over 5 feet
-        this.resultMiller =parseFloat(53.1 + (1.36 * this.calcNum).toFixed(2));
-        //Hamwi formula: 45.5 kg + 2.2 kg per every inch over 5 feet
-        this.resultHamwi =parseFloat(45.5 + (2.2 * this.calcNum).toFixed(2)) ;
+        this.resultMiller = 53.1 + (1.36 * this.calcNum);
+        this.resultMiller = parseFloat(this.resultMiller.toFixed(2));
+        // Robinson formula: 49 kg + 1.7 kg per every inch over 5 feet
+        this.resultRobinson = 49.0 + (1.7 * this.calcNum);
+        this.resultRobinson = parseFloat(this.resultRobinson.toFixed(2));
         //Devine formula: 45.5 kg + 2.3 kg per every inch over 5 feet
-        this.resultDevine =parseFloat(45.5 + (2.3 * this.calcNum).toFixed(2));
-        this.tooLow = `${this.calcNum}`;
+        this.resultDevine = 45.5 + (2.3 * this.calcNum);
+        this.resultDevine = parseFloat(this.resultDevine.toFixed(2));
+        //Hamwi formula: 45.5 kg + 2.2 kg per every inch over 5 feet
+        this.resultHamwi = 45.5 + (2.2 * this.calcNum);
+        this.resultHamwi = parseFloat(this.resultHamwi.toFixed(2));
+        this.tooLow = `f${this.calcNum}`;
     }
   }
   constructor(public navCtrl: NavController) {}
